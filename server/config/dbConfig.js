fe417b5c8b4900df2bbd4e777bb4b6f819ed4417
indrareddy15 <gantaindrareddy83@gmail.com>
2025-02-25 19:00:02 +0530

    dbConfig fix

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-
-
 const connectDB = () => {
     try {
         mongoose.connect(process.env.MONGO_URI, {
@@ -10,5 +8,13 @@ const connectDB = () => {
             useCreateIndex: true,
         });
         console.log("MongoDB Connected");
+    } catch (error) {
+        console.error("MongoDB Connection Error:", error);
+    } finally {
+        mongoose.connection.on("error", (err) => {
+            console.error("MongoDB Connection Error:", err);
+        });
     }
 }
+
+export default connectDB;
